59bfb9522810e779ef98e39461da64fa2a174e69
kasundiperera2004 <kasundiperera2004@gmail.com>
2026-04-29 15:21:32 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -17,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/items", itemRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 mongoose
   .connect(process.env.MONGO_URI)
